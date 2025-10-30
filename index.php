dbbb37171b7ff1aa76c82295b89162e76bc2828e
Diooo00 <dioputranto0@gmail.com>
2025-10-30 18:51:04 +0700

    paketan CRUD

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=head, initial-scale=1.0">
+    <title>Dashboard</title>
+</head>
+<body>
+    Hello, <?php echo $_POST["username"]; ?> 
+    <!-- key array yang di call harus sama dengan "name" yang ada di input form -->
+</body>
+</html>
